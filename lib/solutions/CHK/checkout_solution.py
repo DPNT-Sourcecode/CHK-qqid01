cfca02a39af34b63d60f26ce047944000b8a2988
sbx_user1051 <sbx_user1051@169.254.104.149>
2019-12-06 12:41:11 +0000

    Fri Dec 06 12:41:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,9 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    Supermarket checkout that calculates the total price of a number of items
+    :param skus:
+    :return:
+    """
+
