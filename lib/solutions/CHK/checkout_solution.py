c578cdfbda68643f8989931c012b98f4036a172c
sbx_user1051 <sbx_user1051@169.254.104.149>
2019-12-06 14:13:40 +0000

    Fri Dec 06 14:13:40 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -32,6 +32,16 @@ def item_promotion_calc(item, promo, shopping_bag):
     if free_item in shopping_bag:
         shopping_bag[free_item] = max(0, shopping_bag[free_item] - shopping_bag[item] // min_no_item)
 
+def special_promotion_calc(items_list, shopping_bag):
+    """
+
+    :param items_list:
+    :param shopping_bag:
+    :return:
+    """
+    no_special_items = shopping_bag['special']
+
+
 def checkout(skus):
     """
     Supermarket checkout that calculates the total price based on different items purchased
@@ -44,14 +54,18 @@ def checkout(skus):
     price_promotion = {'A': [(5, 200), (3, 130)], 'B': [(2, 45)], 'H': [(10, 80), (5, 45)], 'K': [(2, 150)],
                        'P': [(5, 200)], 'Q': [(3, 80)], 'V': [(3, 130), (2, 90)]}
     item_promotion = {'E': (2, 'B'), 'F': (3, 'F'), 'N': (3, 'M'), 'R': (3, 'Q'), 'U': (4, 'U')}
+    #special promo item ordered by max to min price
+    special_promo_items = ['Z', 'S', 'T', 'Y', 'X']
     shopping_bag = {}
-    total_price = 0
     for item in skus:
         shopping_bag[item] = shopping_bag.get(item, 0) + 1
+        if item in special_promo_items:
+            shopping_bag['special'] = shopping_bag.get('special', 0) + 1
     #Update shopping bag based on item promotions
     for item in item_promotion:
         if item in shopping_bag:
             item_promotion_calc(item, item_promotion[item], shopping_bag)
+    total_price = special_promotion_calc(special_promo_items, shopping_bag)
     for item in shopping_bag:
         if item in price_promotion:
             total_price += price_promotion_calc(price_promotion[item], shopping_bag[item], 0, prices[item])
@@ -62,6 +76,7 @@ def checkout(skus):
                 return -1
 
     return total_price
+
 
 
 
