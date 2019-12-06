62b95e3eda251a4768cb30e22d1c56ad3b3ab67a
sbx_user1051 <sbx_user1051@169.254.104.149>
2019-12-06 13:31:09 +0000

    Fri Dec 06 13:31:09 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,23 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+def promotion_price_calc(promotion_list, no_item, total_price, unit_price):
+    """
+    Calculate the price based on the various promotions
+    :param promotion_list: list of promotions to apply
+    :param no_item: no items purchased
+    :param total_price: current total price\
+    :param unit_price: price of one unit
+    :return: total price based on promotions
+    """
+    if promotion_list:
+        no_item_promo, price_promo = promotion_list.pop(0)
+        no_promo_applied = no_item // no_item_promo
+        total_price += no_promo_applied * price_promo
+        return promotion_price_calc(promotion_list, no_item - no_promo_applied * no_item_promo, total_price, unit_price)
+    else:
+        return total_price + no_item * unit_price
+
 def checkout(skus):
     """
     Supermarket checkout that calculates the total price of a number of items
@@ -9,17 +26,17 @@ def checkout(skus):
     :return:
     """
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
-    promotion = {'A': (3, 130), 'B': (2, 45)}
+    promotion = {'A': [(5, 200), (3, 130)], 'B': [(2, 45)]}
     shopping_bag = {}
     total_price = 0
     for item in skus:
         shopping_bag[item] = shopping_bag.get(item, 0) + 1
-        if item == 'E':
-            shopping_bag['B'] = shopping_bag.get('B', 0) - 0.5
+    # You get one free B for 2 E bought
+    if 'B' in shopping_bag:
+        shopping_bag['B'] = max(0, shopping_bag['B'] - shopping_bag.get('E', 0) // 2)
     for item in shopping_bag:
         if item in promotion:
-            no_promotion = shopping_bag[item] // promotion[item][0]
-            total_price += no_promotion * promotion[item][1] + (shopping_bag[item] - promotion[item][0] * no_promotion) * prices[item]
+            total_price = promotion_price_calc(promotion[item], shopping_bag[item], 0, prices[item])
         else:
             if item in prices:
                 total_price += shopping_bag[item] * prices[item]
@@ -27,3 +44,4 @@ def checkout(skus):
                 return -1
 
     return total_price
+
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -5,5 +5,12 @@ class TestChk():
     def test_chk(self):
         assert checkout_solution.checkout('AAAABBCD') == 260
 
+    def test_chk_promoEB(self):
+        assert checkout_solution.checkout('AAAABBCDEE') == 325
+
+    def test_chk_promo9A(self):
+        assert checkout_solution.checkout('AAAAAAAAABBCDEE') == 525
+
     def test_chk_illegal_input(self):
         assert checkout_solution.checkout('AAAABEECD') == -1
+
