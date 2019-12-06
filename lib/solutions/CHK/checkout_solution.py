71f9dc54e1d528982dcbf1ccf3fc3871fd791de5
sbx_user1051 <sbx_user1051@169.254.104.149>
2019-12-06 13:01:11 +0000

    Fri Dec 06 13:01:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,4 +8,17 @@ def checkout(skus):
     :param skus:
     :return:
     """
-    print(skus)
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    promotion = {'A': (3, 130), 'B': (2, 45)}
+    shopping_bag = {}
+    total_price = 0
+    for item in skus:
+        shopping_bag[item] += shopping_bag.get(item, 0) + 1
+    for item in shopping_bag:
+        if item in promotion:
+            no_promotion = shopping_bag[item] // promotion[item][0]
+            total_price += no_promotion * promotion[item][1] + (shopping_bag[item] - promotion[item][0] * no_promotion) * prices[item]
+        else:
+            total_price += shopping_bag[item] * prices[item]
+
+    return total_price
---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,6 @@
+from solutions.CHK import checkout_solution
+
+
+class TestChk():
+    def test_chk(self):
+        assert checkout_solution.checkout('AAAABBCD') == 260
